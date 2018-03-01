20b1cd0971dbc49281ec5ab397b261553c32c860
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2018-03-01 19:38:59 +0530

    Added html table

--- signal-r-client/src/Components/About.js
+++ signal-r-client/src/Components/About.js
@@ -6,6 +6,9 @@ class About extends React.Component{
             <h1>
                 About
             </h1>
+            <div>
+                This is an example application to demonstrate usage of signalR with react & redux
+            </div>
         </div>
     }
 }
--- signal-r-client/src/Components/Employees.js
+++ signal-r-client/src/Components/Employees.js
@@ -5,11 +5,25 @@ class Employees extends React.Component {
 
     render() {
         return <div>
-            {this.props.employees ? (this.props.employees.map(emp =>
-                <div key={emp.id}>{emp.id} : {emp.name} - {emp.age} years</div>
-            )) : <div>
-                No records
-            </div>}
+            {this.props.hasData ? <table>
+                <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>Age</th>
+                </tr>
+                </thead>
+                <tbody>
+                {
+                    this.props.employees.map((emp, index) => <tr key={index}>
+                        <td>{emp.id}</td>
+                        <td>{emp.name}</td>
+                        <td>{emp.age}</td>
+                    </tr>)
+                }
+                </tbody>
+            </table> : <div> No records </div>
+            }
         </div>
     }
 }
@@ -17,7 +31,8 @@ class Employees extends React.Component {
 const mapStateToProps = (state) => {
     console.info(state);
     return {
-        employees : state.employees
+        employees: state.employees,
+        hasData: state.employees && state.employees.length > 0
     };
 };
 
